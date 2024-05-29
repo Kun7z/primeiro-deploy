3022e1061275abad1b07862ec6e1153564e85e5f
Kun7z <ematheuskuntz@gmail.com>
2024-05-29 15:45:39 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv").config();
+const express = require('express')
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    return res.json('api ok')
+})
+
+app.listen(process.env.PORT);
